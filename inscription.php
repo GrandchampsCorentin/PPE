933524ce4b1d6827bb927a379be34a5652cdb5a6
GrandchampsCorentin <32384100+GrandchampsCorentin@users.noreply.github.com>
2017-11-09 11:46:39 +0100

    Add files via upload

---
+++ inscription.php
@@ -0,0 +1,22 @@
+<h1>Inscription</h1>
+
+<form action="Controleurs/gestionInscription.php" method="get">
+    <label for="nomDemandeur">Nom : </label>
+    <input type="text" name="nomDemandeur" id="nomDemandeur" value="">
+    <label for="prenomDemandeur">Prénom : </label>
+    <input type="text" name="prenomDemandeur" id="prenomDemandeur" value="">
+    <label for="adresseDemandeur">Adresse : </label>
+    <input type="text" name="adresseDemandeur" id="adresseDemandeur">
+    <label for="cpDemandeur">Code Postal : </label>
+    <input type="text" name="" id="" value="">
+    <label for="villeDemandeur">Ville : </label>
+    <input type="text" name="" id="" value="">
+    <label for="emailDemandeur">Email : </label>
+    <input type="text" name="" id="" value="">
+    <label for="motDePasseDemandeur">Mot de passe : </label>
+    <input type="text" name="" id="" value="">
+    <label for="motDePasseDemandeur2">Mot de passe : </label>
+    <input type="text" name="" id="" value="">
+      
+    <input type="submit" value="S'inscrire" id="bt-inscription">
+</form>
